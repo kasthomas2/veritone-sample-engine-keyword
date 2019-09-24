219f17870882cac332eaa95d74630dba6be1d803
Kas Thomas <47995649+kasthomas2@users.noreply.github.com>
2019-09-24 10:23:56 -0700

    Remove logpoints, profiling calls

--- index.js
+++ index.js
@@ -6,16 +6,9 @@ const KeywordExtraction = require("./keyword-extraction.js"),
     upload = multer({storage: multer.memoryStorage()}),
     app = express();
 
-const PORT = 8080;
-const TIMEOUT = 10 * 60 * 1000;
-
 let chunkUpload = upload.single('chunk');
-let server = app.listen( PORT );
-server.setTimeout( TIMEOUT );
-
-let timingStart = 1 * new Date;
-
-function timing( startTime ) { return (1 * new Date) - startTime; }
+let server = app.listen( 8080 );
+server.setTimeout( 10 * 60 * 1000 );
 
 // READY WEBHOOK
 app.get('/readyz', (req, res) => {
@@ -24,22 +17,10 @@ app.get('/readyz', (req, res) => {
 
 // PROCESS WEBHOOK
 app.post('/process', chunkUpload, async (req, res)=>{
-
     try {
         let buffer = req.file.buffer.toString();
-        // console.log("\nBody was: \n" + JSON.stringify(req.body,null,4));
-
-        timingStart = 1 * new Date;
         let output = KeywordExtraction.getOutput( buffer, null );
-        let elapsed = timing(timingStart);
-        var totalwords = buffer.split(" ").length;
-        var rate = (totalwords/elapsed).toPrecision(4);
-        console.log( "Processing rate was " + rate  + " words per ms, with a sample of N=" + totalwords + " words." );
-        console.log("Elapsed time: " + elapsed + " ms");
-
-        res.header("Content-Type", "application/json");
         return res.status(200).send( output );
-
     } catch (error) {
         return res.status(500).send(error);
     }
